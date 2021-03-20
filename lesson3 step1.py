eb527e5dcd5574876dc6d84d88b10def10cc3271
Ivan <ifedianin@wowworks.ru>
2021-03-20 08:28:06 +0400

    выполнил задания 3.1 по 3.4

--- lesson3 step1.py
+++ lesson3 step1.py
@@ -1,1 +1,12 @@
-assert abs(-42) == -42, "Should be absolute value of a number" #через запятую выводится сообщение об ошибке
+#assert abs(-42) == -42, "Should be absolute value of a number" #через запятую выводится сообщение об ошибке
+
+#print("Let's count together: {}, then goes {}, and then {}".format("one", "два", "three"))
+
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+actual_result = "abrakadabra"
+print(f"Wrong text, got {actual_result}, something wrong")
+
